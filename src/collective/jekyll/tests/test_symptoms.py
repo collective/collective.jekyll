dde98642be469db94b73328e7db87a61b80bbf85
Godefroid Chapelle <gotcha@bubblenet.be>
2012-03-19 09:02:17 +0100

    add first symptom tests

---
+++ src/collective/jekyll/tests/test_symptoms.py
@@ -0,0 +1,19 @@
+import unittest2 as unittest
+
+
+class CountWords(unittest.TestCase):
+
+    def test_no_words(self):
+        from collective.jekyll.symptoms import countWords
+
+        self.assertEquals(0, countWords(u"in the end"))
+
+    def test_one_word(self):
+        from collective.jekyll.symptoms import countWords
+
+        self.assertEquals(1, countWords(u"in the wild"))
+
+    def test_two_words(self):
+        from collective.jekyll.symptoms import countWords
+
+        self.assertEquals(2, countWords(u"into the wild"))

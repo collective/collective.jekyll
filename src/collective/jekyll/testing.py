b6ae7c81122ac2c74c986fb238dcb941b6d26597
Godefroid Chapelle <gotcha@bubblenet.be>
2012-06-05 16:57:42 +0200

    use plone.act pybot.cfg

--- src/collective/jekyll/testing.py
+++ src/collective/jekyll/testing.py
@@ -1,3 +1,4 @@
+from plone.testing import z2
 from plone.app.testing import PloneWithPackageLayer
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import FunctionalTesting
@@ -6,11 +7,15 @@ import collective.jekyll
 
 
 COLLECTIVE_JEKYLL = PloneWithPackageLayer(zcml_package=collective.jekyll,
-    zcml_filename='configure.zcml',
-    gs_profile_id='collective.jekyll:default', name='COLLECTIVE_JEKYLL')
+    zcml_filename='testing.zcml',
+    gs_profile_id='collective.jekyll:testing', name='COLLECTIVE_JEKYLL')
 
 COLLECTIVE_JEKYLL_INTEGRATION = IntegrationTesting(bases=(COLLECTIVE_JEKYLL, ),
                        name="COLLECTIVE_JEKYLL_INTEGRATION")
 
 COLLECTIVE_JEKYLL_FUNCTIONAL = FunctionalTesting(bases=(COLLECTIVE_JEKYLL, ),
                        name="COLLECTIVE_JEKYLL_FUNCTIONAL")
+
+COLLECTIVE_JEKYLL_ZSERVER = FunctionalTesting(
+                       bases=(COLLECTIVE_JEKYLL, z2.ZSERVER_FIXTURE),
+                       name="COLLECTIVE_JEKYLL_ZSERVER")

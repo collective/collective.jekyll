9c41b8d51e5af23eefd7dea9e07b8624ccfa3484
Godefroid Chapelle <gotcha@bubblenet.be>
2013-08-27 14:58:24 +0200

    pep8

--- src/collective/jekyll/testing.py
+++ src/collective/jekyll/testing.py
@@ -8,20 +8,28 @@ from plone.app.robotframework.testing import AUTOLOGIN_LIBRARY_FIXTURE
 import collective.jekyll
 
 
-COLLECTIVE_JEKYLL = PloneWithPackageLayer(zcml_package=collective.jekyll,
+COLLECTIVE_JEKYLL = PloneWithPackageLayer(
+    zcml_package=collective.jekyll,
     zcml_filename='testing.zcml',
-    gs_profile_id='collective.jekyll:testing', name='COLLECTIVE_JEKYLL')
+    gs_profile_id='collective.jekyll:testing',
+    name='COLLECTIVE_JEKYLL'
+)
 
-COLLECTIVE_JEKYLL_INTEGRATION = IntegrationTesting(bases=(COLLECTIVE_JEKYLL, ),
-                       name="COLLECTIVE_JEKYLL_INTEGRATION")
+COLLECTIVE_JEKYLL_INTEGRATION = IntegrationTesting(
+    bases=(COLLECTIVE_JEKYLL, ),
+    name="COLLECTIVE_JEKYLL_INTEGRATION"
+)
 
-COLLECTIVE_JEKYLL_FUNCTIONAL = FunctionalTesting(bases=(COLLECTIVE_JEKYLL, ),
-                       name="COLLECTIVE_JEKYLL_FUNCTIONAL")
+COLLECTIVE_JEKYLL_FUNCTIONAL = FunctionalTesting(
+    bases=(COLLECTIVE_JEKYLL, ),
+    name="COLLECTIVE_JEKYLL_FUNCTIONAL"
+)
 
 COLLECTIVE_JEKYLL_ZSERVER = FunctionalTesting(
-                       bases=(COLLECTIVE_JEKYLL, z2.ZSERVER_FIXTURE),
-                       name="COLLECTIVE_JEKYLL_ZSERVER")
+    bases=(COLLECTIVE_JEKYLL, z2.ZSERVER_FIXTURE),
+    name="COLLECTIVE_JEKYLL_ZSERVER"
+)
 
 COLLECTIVE_JEKYLL_ROBOT_TESTING = FunctionalTesting(
-    bases=(COLLECTIVE_JEKYLL, AUTOLOGIN_LIBRARY_FIXTURE, z2.ZSERVER),
+    bases=(COLLECTIVE_JEKYLL, AUTOLOGIN_LIBRARY_FIXTURE, z2.ZSERVER_FIXTURE),
     name="COLLECTIVE_JEKYLL_ROBOT_TESTING")
